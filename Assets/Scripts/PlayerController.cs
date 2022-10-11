089c69c5cccde1de7d62ae5dbae04b853ca4187a
juneisjune <yhs1535@naver.com>
2022-10-11 23:58:52 +0900

    10월 11일
    함수들 update에 다 넣기

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -57,129 +57,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() //1초에 60번 정도실행됨
     {
-        IsGround();
-        TryJump();
-        TryRun();
-        TryCrouch();
-        Move();
-        CameraRotation();
-        CharacterRotation();
-    }
-    private void TryCrouch()
-    {
-        if(Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            Crouch();
-        }
-    }
-    private void Crouch()
-    {
-        isCrouch = !isCrouch;
-
-        if(isCrouch)
-        {
-            applySpeed = crouchSpeed;
-            applyCrouchPosY=crouchPosY;
-        }
-        else
-        {
-            applySpeed = walkSpeed;
-            applyCrouchPosY=originPosY;
-        }
-        StartCoroutine(CrouchCoroutine());
-    }
-    //부드러운 앉기
-    IEnumerator CrouchCoroutine()
-    {
-        float _posY = theCamera.transform.localPosition.y;
-        int count = 0;
-        while (_posY != applyCrouchPosY) 
-        {
-            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.1f);
-            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
-            if (count > 15)
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {   /*
+            if (isCrouch) //앉은상태 점프 해제
             {
-                break;
-            }
-          
-            yield return null;  //wait one frame
+                Crouch();
+            }*/
+            myRigid.velocity = transform.up * jumpForce;
         }
-        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
-    }
-    //지면 체크
-    private void IsGround()
-    {
-        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y+0.1f);
-    }
-    }
-    private void TryJump()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)&& isGround)
+        if(Input.GetKey(KeyCode.LeftShift))
         {
-            Jump();
+            isRun= true;
+            applySpeed = runSpeed;
         }
     }
-    private void Jump()
-    {
-        if(isCrouch) //앉은상태 점프 해제
-        {
-            Crouch();
-        }
-        myRigid.velocity = transform.up * jumpForce;
-    }
 
-    private void TryRun()
-    {
-        if(Input.GetKey(KeyCode.LeftShift)) //when player pressing leftshift return true
-        {
-            Running();
-        }
-        if(Input.GetKeyUp(KeyCode.LeftShift)) 
-        {
-            RunningCancel();
-        }
-    }
-    private void Running()
-    {
-        if (isCrouch) 
-        {
-            Crouch();
-        }
-        isRun = true;
-         applySpeed = runSpeed;
-    }
-    private void RunningCancel()
-    {
-        isRun=false;
-        applySpeed=walkSpeed;
-    }
-    private void Move()
-    {
-        float _moveDirX = Input.GetAxisRaw("Horizontal");
-        float _moveDirZ = Input.GetAxisRaw("Vertical");
-
-        Vector3 _moveHorizontal = transform.right * _moveDirX;
-        Vector3 _moveVertical = transform.forward * _moveDirZ;
-
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
-
-        //transform.position(현재위치)에서 velocity(속도)만큼 더해줌 이러면 순간이동함으로 deltaTime으로 나눠줌 Time.deltaTime=0.016
-        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime); 
-    }
-    private void CharacterRotation() //좌우 마우스
-    {
-        float _yRotation = Input.GetAxisRaw("Mouse X");
-        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
-
-        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
-    }
-    private void CameraRotation() //상하 마우스
-    {
-        float _xRotation = Input.GetAxisRaw("Mouse Y");
-        float _cameraRotationX = _xRotation * lookSensitivity; //현재 위치에서 민감도만큼 곱해주기
-        currentCameraRotationX -= _cameraRotationX;
-        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);//카메라의 최대값과 최소값 유지
-
-        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);    
-    }
 }
